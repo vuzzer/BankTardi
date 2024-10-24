b024ee62a9f73dceb20e59ca111247580d0c83e9
Vuzzer <parfaitkouess@gmail.com>
2024-10-24 14:52:10 -0400

    Add WeatherForest and modify path of backend service

--- Controllers/TransfertController.cs
+++ Controllers/TransfertController.cs
@@ -1,3 +1,4 @@
+using BanqueTardi.Models;
 using Dapr.Client;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,8 +13,8 @@ namespace BanqueTardi.Controllers
             _httpClient = DaprClient.CreateInvokeHttpClient("transfert");
         }
 
-        public async Task<ActionResult<object>> SendMoney(){  
-            var response = await _httpClient.GetFromJsonAsync<object>("TransfertMoney");
+        public async Task<ActionResult<IEnumerable<WeatherForecast>>> SendMoney(){  
+            var response = await _httpClient.GetFromJsonAsync<WeatherForecast[]>("api/Transaction/TransfertMoney");
             return response;
         }
     }
